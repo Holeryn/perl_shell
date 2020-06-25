d4b2ac33cc3e290795848782cbe52f9bca793073
Holeryn <Holeryn@protonmail.com>
2020-06-25 21:37:35 +0200

    Disabled stdout buffering and changed the method for read input

--- perl_shell.pl
+++ perl_shell.pl
@@ -5,6 +5,9 @@ use warnings;
 &main();
 
 sub main {
+    # Disable stdout buffering
+    $| = 1;
+
     my $input;
     print("Perl shell , exit for terminate the program\n");
 
@@ -17,10 +20,15 @@ sub main {
 sub read_input {
     print(">>> ");
     # Read input
-    my $input = <STDIN>;
+    my $input;
+    do{
+        $input .= getc(STDIN);
+    }until($input =~ /.+\n$/); # Check if the input end with \n
+
+    print $input;
     my $result = eval($input);
 
-    # If the input is a not valid comand
+    # If the input is a not valid comand print the error
     unless($result){
         print($@);
     }
